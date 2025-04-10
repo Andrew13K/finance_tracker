d4e5a2efe853f96d622c610f2722c5993620fe07
dreymen <andreykosmenuk13@gmail.com>
2025-04-10 15:41:22 +0200

    Add constructor to connect and destructor to disconnect from db

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,44 @@
 #include <mysql/mysql.h>
 #include <string>
 
+using namespace std;
+
 class MySQLConnection{
     private:
         MYSQL* conn;
-        std::string host;
-        std::string user;
-        std::string password;
-        std::string database;
+        string host;
+        string user;
+        string password;
+        string database;
         int port;
         bool connected;
+    public:
+        MySQLConnection(
+            const string& host = "localhost",
+            const string& user = "username",
+            const string& password = "password",
+            const string& database = "db_name",
+            int port = 3306
+        ):host(host),user(user),password(password),database(database),
+        port(port),connected(false){
+            conn = mysql_init(NULL);
+            if(conn == NULL)
+                cerr<<"MySQL initialization failed"<<endl;
+        }
 
+        ~MySQLConnection(){
+            disconnect();
+        }
+
+        void disconnect()
+        {
+            if(connected && conn)
+            {
+                mysql_close(conn);
+                connected = false;
+                cout<<"Disconnected from MySQL database"<<endl;
+            }
+        }
 };
 
 int main()
