70346a786ccf024d633fb1c80cfbc2a985b1d8a3
dreymen <andreykosmenuk13@gmail.com>
2025-04-22 17:55:01 +0200

    Add 3 buttons in the bottom navbar

--- include/frame_dashboard.hpp
+++ include/frame_dashboard.hpp
@@ -1,18 +1,29 @@
 #ifndef DASH_HPP
 #define DASH_HPP
 
-#include "mysql_connection.hpp"
+#include <wx/statline.h>
 #include <wx/wx.h>
+#include "mysql_connection.hpp"
 
 class DashboardFrame: public wxFrame {
     public:
         DashboardFrame(const wxString& title, const wxString& name, MySQLConnection& db);
         ~DashboardFrame();
-        void OnClose(wxCloseEvent& evt);
     private:
+        void OnClose(wxCloseEvent& evt);
+
+        // UI Elements
         wxPanel* panel;
-        MySQLConnection& db;
         wxStaticText* welcomeText;
+        wxBoxSizer* navSizer;
+
+        // Bottom nav buttons
+        wxButton* btnProfile;
+        wxButton* btnHistory;
+        wxButton* btnFriends;
+
+        // DB reference
+        MySQLConnection& db;
 };
 
 #endif
--- src/frame_dashboard.cpp
+++ src/frame_dashboard.cpp
@@ -14,15 +14,51 @@ DashboardFrame::DashboardFrame(const wxString& title, const wxString& name, MySQ
 
     wxBoxSizer* vbox = new wxBoxSizer(wxVERTICAL);
 
-    welcomeText = new wxStaticText(panel, wxID_ANY, welcomeMessage, wxPoint(50,50), wxDefaultSize);
+    welcomeText = new wxStaticText(panel, wxID_ANY, welcomeMessage);
     wxFont font = welcomeText->GetFont();
     font.SetPointSize(18);
     font.SetWeight(wxFONTWEIGHT_BOLD);
     welcomeText->SetFont(font);
-    Center();
 
-    vbox->Add(welcomeText, 0, wxALIGN_CENTER | wxTOP | wxBOTTOM, 30);\
+    vbox->Add(welcomeText, 0, wxALIGN_CENTER | wxTOP | wxBOTTOM, 30);
+
+    vbox->AddStretchSpacer(1);
+
+    wxPanel* navPanel = new wxPanel(panel, wxID_ANY);
+    navPanel->SetBackgroundColour(wxColour(240, 240, 240)); // light gray bg like Telegram
+    
+    // Add a border line on top
+    navPanel->SetBackgroundStyle(wxBG_STYLE_PAINT);
+    navPanel->Bind(wxEVT_PAINT, [](wxPaintEvent& evt) {
+        wxPaintDC dc(static_cast<wxPanel*>(evt.GetEventObject()));
+        wxSize size = dc.GetSize();
+        dc.SetPen(wxPen(wxColour(200, 200, 200))); // Light gray line
+        dc.DrawLine(0, 0, size.GetWidth(), 0); // top border
+    });
+    
+    wxBoxSizer* navSizer = new wxBoxSizer(wxHORIZONTAL);
+    
+    wxButton* btnProfile = new wxButton(navPanel, wxID_ANY, "Profile");
+    wxButton* btnHistory = new wxButton(navPanel, wxID_ANY, "History");
+    wxButton* btnFriends = new wxButton(navPanel, wxID_ANY, "Friends");
+    
+    // Stretch evenly between buttons - using proportion 1 for each button
+    navSizer->Add(btnProfile, 1, wxEXPAND | wxALL, 5);
+    navSizer->Add(new wxStaticLine(navPanel, wxID_ANY, wxDefaultPosition, wxSize(1, -1), wxLI_VERTICAL), 
+                 0, wxEXPAND | wxTOP | wxBOTTOM, 5);
+    navSizer->Add(btnHistory, 1, wxEXPAND | wxALL, 5);
+    navSizer->Add(new wxStaticLine(navPanel, wxID_ANY, wxDefaultPosition, wxSize(1, -1), wxLI_VERTICAL), 
+                 0, wxEXPAND | wxTOP | wxBOTTOM, 5);
+    navSizer->Add(btnFriends, 1, wxEXPAND | wxALL, 5);
+    
+    navPanel->SetSizer(navSizer);
+    
+    // Add the nav panel with wxEXPAND flag to stretch it horizontally
+    vbox->Add(navPanel, 0, wxEXPAND);
+    
     panel->SetSizer(vbox);
+    
+    Center();
     this->Bind(wxEVT_CLOSE_WINDOW, &DashboardFrame::OnClose, this);
 }
 
